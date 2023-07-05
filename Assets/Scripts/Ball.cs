af6db3180d29c29f02fbdaba33301f9e85b4d7ec
SaharGGJ <124356732+SaharGGJ@users.noreply.github.com>
2023-07-05 20:48:45 +0300

    I tried

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -21,4 +21,22 @@ public class Ball : MonoBehaviour
     {
         Rigidbody2D.velocity = new Vector3(x, y, 0);
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag.Equals("Top Border")|| collision.gameObject.tag.Equals("Bottom Border"))
+        {
+            Rigidbody2D.velocity = new Vector3(x,y*-5,0); 
+        }
+        if(collision.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("Hit player");
+            if(gameObject.transform.position.x>Screen.width/2)
+            {
+                Rigidbody2D.velocity = new Vector3(-1,y,0);
+            }
+            else
+                Rigidbody2D.velocity = new Vector3(1, y, 0);
+            Rigidbody2D.velocity = new Vector3(-1*x, y, 0);
+        }
+    }
 }
