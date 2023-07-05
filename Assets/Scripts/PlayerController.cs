f82b6546289f98356c886e8ec7d1c58de3356293
JoyBoy <barbetzalel412@gmail.com>
2023-07-05 20:42:52 +0300

    did i break everything?

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] GameObject playerOneBoard;
+    [SerializeField] private AudioSource playerOneAudioSource;
+    public float speedMod = 1;
     private bool touchingTop = false;
     private bool touchingBottom = false;
     // Update is called once per frame
@@ -12,20 +14,28 @@ public class PlayerController : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.UpArrow)&&!touchingTop)
         {
-            playerOneBoard.transform.Translate(Vector3.up * Time.deltaTime);
+            playerOneBoard.transform.Translate(Vector3.up * Time.deltaTime*speedMod);
         }
         else
         if (Input.GetKey(KeyCode.DownArrow)&&!touchingBottom)
         {
-            playerOneBoard.transform.Translate(Vector3.down * Time.deltaTime);
+            playerOneBoard.transform.Translate(Vector3.down * Time.deltaTime * speedMod);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag.Equals("Top Border"))
-            touchingTop= true;
-        if (collision.gameObject.tag.Equals("Bottom Border"));
-            touchingBottom= true;
+        if (collision.gameObject.tag.Equals("Top Border"))
+        {
+            touchingTop = true;
+            Debug.Log("Hitting top!");
+        }
+        if (collision.gameObject.tag.Equals("Bottom Border"))
+        {
+            touchingBottom = true;
+            Debug.Log("Hitting Bottom!");
+        }
+        if (collision.gameObject.tag.Equals("Ball"))
+            playerOneAudioSource.Play();
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
--- Assets/Scripts/PlayerTwoController.cs
+++ Assets/Scripts/PlayerTwoController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerTwoController : MonoBehaviour
 {
     [SerializeField] private GameObject playerTwoBoard;
+    [SerializeField] private AudioSource playerTwoAudioSource;
+    public float speedMod = 1;
     private bool touchingTop = false;
     private bool touchingBottom = false;
     // Update is called once per frame
@@ -12,20 +14,28 @@ public class PlayerTwoController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W) && !touchingTop)
         {
-            playerTwoBoard.transform.Translate(Vector3.up * Time.deltaTime);
+            playerTwoBoard.transform.Translate(Vector3.up * Time.deltaTime * speedMod);
         }
         else
         if (Input.GetKey(KeyCode.S) && !touchingBottom)
         {
-            playerTwoBoard.transform.Translate(Vector3.down * Time.deltaTime);
+            playerTwoBoard.transform.Translate(Vector3.down * Time.deltaTime * speedMod);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag.Equals("Top Border"))
+        {
             touchingTop = true;
-        if (collision.gameObject.tag.Equals("Bottom Border")) ;
-        touchingBottom = true;
+            Debug.Log("Hitting top!");
+        }
+        if (collision.gameObject.tag.Equals("Bottom Border"))
+        {
+            touchingBottom = true;
+            Debug.Log("Hitting Bottom!");
+        }
+        if(collision.gameObject.tag.Equals("Ball"))
+            playerTwoAudioSource.Play();
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
